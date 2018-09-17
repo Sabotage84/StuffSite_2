998d80727b654d36d0c6009ae4c894e1d27696e5
sabotage84@mail.ru <sabotage84@mail.ru>
2018-09-17 17:10:23 +0300

    Try add DB, but unsuccessfully until

---
+++ RukiKrukiWithUsers/Models/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RukiKrukiWithUsers.Models
+{
+    public class Product
+    {
+        string name;
+
+        int iD;
+
+        string image;
+
+        string description;
+
+        double price;
+
+        bool isInStock;
+
+        public string Name { get => name; set => name = value; }
+        public string Description { get => description; set => description = value; }
+        public double Price { get => price; set => price = value; }
+        public bool IsInStock { get => isInStock; set => isInStock = value; }
+        public string Image { get => image; set => image = value; }
+        public int ID { get => iD; set => iD = value; }
+
+
+    }
+}
---
+++ RukiKrukiWithUsers/Models/Videos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RukiKrukiWithUsers.Models
+{
+    public class Videos
+    {
+
+        private string name;
+
+        public string Name { get { return name; } set => name = value; }
+
+        public string UrlVideo { get { return urlVideo; } set => urlVideo = value; }
+
+        private string urlVideo;
+
+    }
+}
